e781e4b58b52a30676f4ca360712d94178a5c7fd
Jonghan Park <jonghan.park@edu.sait.ca>
2022-08-19 09:02:40 -0600

    Update backend

---
+++ server/index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST);
+const bodyParser = require("body-parser");
+const cors = require("cors");
